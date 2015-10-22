0edeb886211b66dbdcdd55ae51fd8e84c860657b
Joe Audette <joeaudette@users.noreply.github.com>
2015-10-22 13:58:08 -0400

    add localization test project

---
+++ LocalizationWebSite/Startup.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace LocalizationWebSite
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddViewLocalization(options => options.ResourcesPath = "Resources");
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            //app.UseCultureReplacer();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseIISPlatformHandler();
+
+            app.UseRequestLocalization();
+            
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(context =>
+            {
+                context.Response.StatusCode = 404;
+                
+                return Task.FromResult(0);
+            });
+        }
+    }
+}
